c159a90ecf9aff058cf5507485d9f3637bfd5046
Janio Isacura <janioisacura@gmail.com>
2020-11-10 21:39:34 -0300

    Pipes decimal/number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitán América';
-  arreglo: number[] = [1,2,3,4,5,6,7,8,9,10];
+  arreglo:string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI: number = Math.PI;
 }
