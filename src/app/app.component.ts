4acaff4cc4fa842289733abe1ef797f9f961b323
Janio Isacura <janioisacura@gmail.com>
2020-11-11 22:36:05 -0300

    Pipe porcentaje agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   nombre: string = 'Capitán América';
   arreglo:string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
   PI: number = Math.PI;
+  porcentaje: number = 0.234;
 }
